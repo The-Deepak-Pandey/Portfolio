798ece3d663d9b80f9857e6bfaefe8f205189220
The-Deepak-Pandey <deepak.pandey@students.iiit.ac.in>
2025-01-17 00:58:05 +0530

    typing animation done

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// TYPING ANIMATION
+
+// var typed = new Typed('.typing', {
+//     strings: ["Full Stack Developer", "Competitive Programmer", "Web Designer", "Photographer", "Freelancer"],
+//     typeSpeed: 100,
+//     backSpeed: 60,
+//     loop: true
+// })
